9143b5f354913910b07266cad61a91a5f24fd9d1
Nadejda <nadyash295923@gmail.com>
2024-04-11 14:51:00 +0300

    update

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,11 +2,20 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
-
-        switch (year % 4)
+        switch(year % 400)
         {
-            case 0 : System.out.println("leap");
-            default: System.out.println("not leap");
+            case 0: System.out.println("leap"); break;
+            default:
+                switch (year % 4)
+                {
+                    case 0:
+                        switch (year % 100) {
+                            case 0: System.out.println("not leap");
+                            default: System.out.println("leap");
+                    }
+                    default: System.out.println("not leap");
+            }
         }
+
     }
 }
